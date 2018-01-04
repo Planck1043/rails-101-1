37556c8dfe9a7907eeaea64e4cf798e4ebda229c
Planck1043 <zhao1036713209@gmail.com>
2018-01-04 16:08:41 +0800

    测试01

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'groups#index'
+  resources :welcome
   devise_for :users
   resources :groups do
     member do
@@ -14,6 +16,5 @@ Rails.application.routes.draw do
     resources :posts
   end
 
-  root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
